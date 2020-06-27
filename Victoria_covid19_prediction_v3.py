95a5a1795f210cee35be08590370822311edd242
Data Science | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Python | SAS | R <sauldata@tutanota.com>
2020-06-27 17:09:37 +1000

    Add files via upload

--- Victoria_covid19_prediction_v3.py
+++ Victoria_covid19_prediction_v3.py
@@ -6,6 +6,8 @@ from datetime import datetime
 import os
 import re
 
+#Global variables
+R_T_MAX = 6
 teststartdate = '2020-01-25'
 period = 14 # moving average period is 14 days
 figsize = (1500 / 50, 400 / 50)
@@ -42,6 +44,84 @@ def getVicdata():
     movingAverage=plotVicCov19(flattened)
 
     print(" Len Moving Averages", len(movingAverage))
+
+    #posteriors, log_likelihood = get_posteriors(movingAverage, AUdailytests['newcasestotalratio'], sigma=.25)
+    get_posteriors(movingAverage, 0.25)
+
+
+def get_posteriors(ma, newtotalratio = [1,1], sigma=0.15):
+
+    print(" Len Moving Averages", len(ma))
+    print(" Len newtotalratio", len(newtotalratio))
+    print("Sigma ", sigma)
+
+
+    # We create an array for every possible value of Rt
+
+    r_t_range = np.linspace(0, R_T_MAX, len(newtotalratio))
+    #r_t_range = np.linspace(0, R_T_MAX, R_T_MAX * 10 + 5)
+    ma = ma.cases # get new cases to be used in the lambda calculation
+
+    #print(" Cases Moving Averages", ma)
+    #print("New total ratio ", newtotalratio)
+
+    # (1) Calculate Lambda
+    sumtwovecs = np.exp(GAMMA * ((r_t_range[:, None] - 1)))
+
+    #previous model
+    #lam = ma[:-1].values * sumtwovecs
+
+    #improved model
+    lam = ma[:-1].values * sumtwovecs + newtotalratio[:, None]
+
+    # (2) Calculate each day's likelihood
+    likelihoods = pd.DataFrame(
+        data=sps.poisson.pmf(ma[1:].values, lam),
+        index=r_t_range,
+        columns=ma.index[1:])
+
+    # (3) Create the Gaussian Matrix
+    process_matrix = sps.norm(loc=r_t_range,
+                              scale=sigma
+                              ).pdf(r_t_range[:, None])
+
+    # (3a) Normalize all rows to sum to 1
+    process_matrix /= process_matrix.sum(axis=0)
+
+    # (4) Calculate the initial prior
+    prior0 = np.ones_like(r_t_range) / len(r_t_range)
+
+    # Create a DataFrame that will hold our posteriors for each day
+    # Insert our prior as the first posterior.
+
+    posteriors = pd.DataFrame(
+        index=r_t_range,
+        columns=ma.index,
+        data={ma.index[0]: prior0}
+    )
+
+    # We said we'd keep track of the sum of the log of the probability
+    # of the data for maximum likelihood calculation.
+    log_likelihood = 0.0
+
+    # (5) Iteratively apply Bayes' rule
+    for previous_day, current_day in zip(ma.index[:-1], ma.index[1:]):
+        # (5a) Calculate the new prior
+        current_prior = process_matrix @ posteriors[previous_day]
+
+        # (5b) Calculate the numerator of Bayes' Rule: P(k|R_t)P(R_t)
+        numerator = likelihoods[current_day] * current_prior
+
+        # (5c) Calcluate the denominator of Bayes' Rule P(k)
+        denominator = np.sum(numerator)
+
+        # Execute full Bayes' Rule
+        posteriors[current_day] = numerator / denominator
+
+        # Add to the running sum of log likelihoods
+        log_likelihood += np.log(denominator)
+
+    return posteriors, log_likelihood
 
 def plotVicCov19(flattened):
 
