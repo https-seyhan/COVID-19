35967b89986aa9c88ed0a53f5e83610970dbd587
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-01-09 11:41:07 +1100

    Update COVID-19_Medical_Care_NLP_model.py

--- NLP/COVID-19_Medical_Care_NLP_model.py
+++ NLP/COVID-19_Medical_Care_NLP_model.py
@@ -53,7 +53,7 @@ allWords = []
 medical_care = [['', '', '']]
 
 # Add customised stop word into the NLP model
-# custimize_stop_words = [
+# customize_stop_words = [
 #    'From','from', 'To', 'to', 'Hospital', 'hospital', '-', ')', '(', ',', ':', 'of', 'for', 'the', 'The', 'is',
 #	'[', ']', ';', "\xa0", '/', 'virus', 'studies', '1', 'BACKGROUND', 'population', 'previously', 'countries', 'dogs', 'data',
 #	'infection', '%', 'viral'
