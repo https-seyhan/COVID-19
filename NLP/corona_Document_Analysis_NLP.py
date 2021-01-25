636831819608c2ac5325450f985ffacc018dfa45
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-01-25 14:38:53 +1100

    Update corona_Document_Analysis_NLP.py

--- NLP/corona_Document_Analysis_NLP.py
+++ NLP/corona_Document_Analysis_NLP.py
@@ -69,7 +69,6 @@ for w in customize_stop_words:
     nlp.vocab[w].is_stop = True
 
 os.chdir("/home/saul/corona/CORD-19-research-challenge/2020-03-13")
-
 filename = 'all_sources_metadata_2020-03-13.csv'
 
 
