88a6bcceb5deef3bab3495ddab0826b6926d78ab
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-02-18 16:20:53 +1100

    Update COVID-19_Medical_Care_NLP_model.py

--- NLP/COVID-19_Medical_Care_NLP_model.py
+++ NLP/COVID-19_Medical_Care_NLP_model.py
@@ -80,7 +80,6 @@ def readfile():
     nodupcorona = coronafile.drop_duplicates(subset=None, keep='first', inplace=False)
     print(len(nodupcorona))
     drop_list = ["WHO #Covidence"]
-
     nodupcorona = nodupcorona.drop(drop_list, axis=1)
     analyseAbstract(nodupcorona.sha, nodupcorona.abstract, textcount)
     print("Text Count", textcount)
