f2494f9f69407d1f51a8e9675708ac70c925c27f
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-03-22 21:57:30 +1100

    Update Victoria_covid19_prediction_v9.py

--- Victoria/Victoria_covid19_prediction_v9.py
+++ Victoria/Victoria_covid19_prediction_v9.py
@@ -33,7 +33,6 @@ os.chdir('/home/saul/corona')
 def getVicdata():
     print(f'Victoria')
     dailydata = pd.read_csv('cases_daily_state.csv', parse_dates=['Date'], sep=',')
-
     vicdata = dailydata[['Date', 'VIC']] # keep two variables Date and VIC
 
     vicdata['Date'] = vicdata['Date'].apply(lambda x: x.replace("/", "-"))
