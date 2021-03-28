780d734b04439f281b2df1a1cc680e2a13bcb5b6
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-03-28 17:27:35 +1100

    Update covid19_prediction_Final.py

--- Sydney/covid19_prediction_Final.py
+++ Sydney/covid19_prediction_Final.py
@@ -111,7 +111,6 @@ def get_posteriors(ma, newtotalratio, sigma=0.15):
     print(" Len newtotalratio", len(newtotalratio))
 
     # We create an array for every possible value of Rt
-
     r_t_range = np.linspace(0, R_T_MAX, len(newtotalratio))
     #r_t_range = np.linspace(0, R_T_MAX, R_T_MAX * 10 + 5)
     ma = ma.cases # get new cases to be used in the lambda calculation
