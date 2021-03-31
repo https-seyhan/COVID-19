be7a0c9fcad47173a603837023c7d2277fbb7eb8
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-03-31 21:21:50 +1100

    Update corona_Document_Analysis_NLP.py

--- NLP/corona_Document_Analysis_NLP.py
+++ NLP/corona_Document_Analysis_NLP.py
@@ -185,7 +185,6 @@ def coronaAnalysis(sha, abstract, count, textcount):
 		leftwords.reverse()
 
 		while ((len(cleandoc) >  end + moveright) and (str(cleandoc[end + moveright]) != ".") ):
-
 			#print("Next Word :", cleandoc[end + moveright])
 			moveright = moveright + 1
 			#print("movement :", moveright)
