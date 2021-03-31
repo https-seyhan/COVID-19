0068814539e6381f5abead1353132168d14e1de1
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-03-31 21:05:52 +1100

    Update corona_Document_Analysis_NLP.py

--- NLP/corona_Document_Analysis_NLP.py
+++ NLP/corona_Document_Analysis_NLP.py
@@ -67,7 +67,6 @@ for w in customize_stop_words:
 os.chdir("/home/saul/corona/CORD-19-research-challenge/2020-03-13")
 filename = 'all_sources_metadata_2020-03-13.csv'
 
-
 def readfile():
 	textcount = 0
 	print("reading the file")
