a69ee6d488fa2b5298238d5db614c89c876db73a
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-02 09:35:49 +1100

    Update covid19_prediction_v19.py

--- Sydney/covid19_prediction_v19.py
+++ Sydney/covid19_prediction_v19.py
@@ -22,7 +22,6 @@ keepAU = ['date', 'total_tests', 'new_cases', 'total_cases','population']
 keep = ['date', 'postcode']
 #Daily realtime corona cases data of NSW
 dailycases = "https://data.nsw.gov.au/data/dataset/aefcde60-3b0c-4bc0-9af1-6fe652944ec2/resource/21304414-1ff1-4243-a5d2-f52778048b29/download/covid-19-cases-by-notification-date-and-postcode-local-health-district-and-local-government-area.csv"
-
 dailytests ="https://covid.ourworldindata.org/data/owid-covid-data.csv"
 
 teststartdate = '2020-01-25'
