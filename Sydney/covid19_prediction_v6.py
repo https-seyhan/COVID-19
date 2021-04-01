586d02e892a2128fcb4ffb30e1154301d6b86bea
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-02 09:19:29 +1100

    Update covid19_prediction_v6.py

--- Sydney/covid19_prediction_v6.py
+++ Sydney/covid19_prediction_v6.py
@@ -43,7 +43,6 @@ def statedata(postcode):
     #Allocate count of cases to each postcode
     postcodecases[postcode] = len(statedat)
 
-
 for pcode in range(len(postcodecount)):
     #print("Post code ", postcodecount[pcode])
     statedata(postcodecount[pcode])
