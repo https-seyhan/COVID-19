6d40e05f1f0e0bf781509502a469b80ec50df5d9
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-02 09:29:41 +1100

    Update covid19_prediction_v6.py

--- Sydney/covid19_prediction_v6.py
+++ Sydney/covid19_prediction_v6.py
@@ -339,7 +339,6 @@ def plot_rt(result, ax):
     ax.set_xlim(pd.Timestamp('2020-03-01'), result.index.get_level_values('notification_date')[-1] + pd.Timedelta(days=1))
     fig.set_facecolor('w')
 
-
 fig, ax = plt.subplots(figsize=(600 / 72, 400 / 72))
 
 plot_rt(result, ax)
