43cf8f578ca0638cff9ff732ac9869e974e25586
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-02 09:32:19 +1100

    Update covid19_prediction_v6.py

--- Sydney/covid19_prediction_v6.py
+++ Sydney/covid19_prediction_v6.py
@@ -15,8 +15,7 @@ keep = ['notification_date', 'postcode']
 dailycases = "https://data.nsw.gov.au/data/dataset/aefcde60-3b0c-4bc0-9af1-6fe652944ec2/resource/21304414-1ff1-4243-a5d2-f52778048b29/download/covid-19-cases-by-notification-date-and-postcode-local-health-district-and-local-government-area.csv"
 coronadata = pd.read_csv(dailycases, parse_dates=['notification_date'],
                          squeeze=True, sep=',')
-print(type(coronadata))
-print(type(coronadata.postcode))
+
 #keep notification_date and postcode
 
 print("row count before ", len(coronadata))
