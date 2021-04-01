494fa2d1ff84d3c9248bc2755017ca0837282416
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-02 09:26:45 +1100

    Update covid19_prediction_v6.py

--- Sydney/covid19_prediction_v6.py
+++ Sydney/covid19_prediction_v6.py
@@ -198,7 +198,6 @@ def get_posteriors(ma, sigma=0.15):
 
         # Add to the running sum of log likelihoods
         log_likelihood += np.log(denominator)
-
     return posteriors, log_likelihood
 
 
