2939a1367ad51375c6c3a2feb3429c093a7dfeb0
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-06 20:43:31 +1000

    Update covid19_prediction_v13.py

--- Sydney/covid19_prediction_v13.py
+++ Sydney/covid19_prediction_v13.py
@@ -247,7 +247,6 @@ hdi = highest_density_interval(posteriors, p=.75, debug=True)
 hdis = highest_density_interval(posteriors, p=.75)
 
 most_likely = posteriors.idxmax().rename('ML')
-
 # Look into why you shift -1
 result = pd.concat([most_likely, hdis], axis=1)
 
