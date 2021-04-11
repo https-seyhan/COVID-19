99d2911641616167e96e2c0c31e93c46da9ff9f7
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-12 07:05:55 +1000

    Update covid19_prediction_v18.py

--- Sydney/covid19_prediction_v18.py
+++ Sydney/covid19_prediction_v18.py
@@ -206,8 +206,6 @@ def highest_density_interval(pmf, p, debug=False):
                      index=[f'Low_{p * 100:.0f}',
                             f'High_{p * 100:.0f}'])
 
-
-
 hdi = highest_density_interval(posteriors, p=.9, debug=True)
 
 
