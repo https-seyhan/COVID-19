378e437d408cb359d8cce63dfca8ef4a8fa03680
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-11 12:12:00 +1000

    Update Victoria_covid19_prediction_v8.py

--- Victoria/Victoria_covid19_prediction_v8.py
+++ Victoria/Victoria_covid19_prediction_v8.py
@@ -110,7 +110,6 @@ def highest_density_interval(posteriors, p, debug=False):
 
     cumsum = np.cumsum(posteriors.values)
     
-
     # N x N matrix of total probability mass for each low, high
     total_p = cumsum - cumsum[:, None]
 
