f027c069f0786def97b76aec883628a7a6550af9
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-11 12:08:21 +1000

    Update Victoria_covid19_prediction_v8.py

--- Victoria/Victoria_covid19_prediction_v8.py
+++ Victoria/Victoria_covid19_prediction_v8.py
@@ -63,7 +63,6 @@ def getVicdata():
 
     plotCoeffs(posteriors, hdi, hdis)
     most_likely = posteriors.idxmax().rename('ML')
-
     # Look into why you shift -1
     result = pd.concat([most_likely, hdis], axis=1)
     plot_rt(result)
