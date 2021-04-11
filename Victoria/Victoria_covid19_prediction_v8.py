ccf267fca04b930f7568d6afdccaa756493e9bb3
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-11 12:08:53 +1000

    Update Victoria_covid19_prediction_v8.py

--- Victoria/Victoria_covid19_prediction_v8.py
+++ Victoria/Victoria_covid19_prediction_v8.py
@@ -71,7 +71,6 @@ def plotCoeffs(posteriors, hdi, hdis):
     most_likely = posteriors.idxmax().rename('ML')
     # Look into why you shift -1
     result = pd.concat([most_likely, hdis], axis=1)
-
     most_likely_values = posteriors.idxmax(axis=0)
 
     ax = most_likely_values.plot(marker='o',
