bc94cbdbbb6a970e30a4f2bbd0d52265122fb5dc
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-12 07:06:22 +1000

    Update covid19_prediction_v18.py

--- Sydney/covid19_prediction_v18.py
+++ Sydney/covid19_prediction_v18.py
@@ -194,7 +194,6 @@ def highest_density_interval(pmf, p, debug=False):
     total_p = cumsum - cumsum[:, None]
 
     # Return all indices with total_p > p
-
     lows, highs = (total_p > p).nonzero()
 
     # Find the smallest range (highest density)
