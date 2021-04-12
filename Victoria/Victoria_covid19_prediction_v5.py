e019e9e28867db0fc83340ad15787fac74b1e129
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-13 00:21:59 +1000

    Update Victoria_covid19_prediction_v5.py

--- Victoria/Victoria_covid19_prediction_v5.py
+++ Victoria/Victoria_covid19_prediction_v5.py
@@ -25,7 +25,6 @@ def getVicdata():
     vicdata = dailydata[['Date', 'VIC']] # keep two variables Date and VIC
     vicdata['Date'] = vicdata['Date'].apply(lambda x: x.replace("/", "-"))
 
-
     vicdata['newDate'] = vicdata['Date'].apply(lambda x: str(x) + '-2020')
 
     vicdata['newDate2'] = vicdata['newDate'].apply(lambda x: datetime.strptime(x, '%d-%m-%Y'))
