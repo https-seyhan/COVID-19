ee5a374fc3a49b83352f7d1239a5195333968b1f
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-13 00:07:57 +1000

    Update covid19_prediction_v12.py

--- Sydney/covid19_prediction_v12.py
+++ Sydney/covid19_prediction_v12.py
@@ -64,7 +64,6 @@ print("Coronadata Columns ", coronadata.columns)
 #Clean data
 coronadata = coronadata[(coronadata['postcode'] != 0)]
 coronadata = coronadata[(coronadata['postcode'].notna())]
-
 coronadata = coronadata[keep]
 
 coronadata['date'] = pd.to_datetime(coronadata['date'])
