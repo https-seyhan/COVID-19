34de9dd3933e38e4ccede17ebd8b323f1804d4eb
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-13 00:07:06 +1000

    Update covid19_prediction_v12.py

--- Sydney/covid19_prediction_v12.py
+++ Sydney/covid19_prediction_v12.py
@@ -23,9 +23,6 @@ AUdailytests = AUdailytests[keepAU]
 AUdailytests['date'] = pd.to_datetime(AUdailytests['date'])
 #drop na
 #AUdailytests = AUdailytests[AUdailytests['total_tests'].notna()]
-
-
-
 AUdailytests['new_cases'].astype(float)
 AUdailytests['DeltaCase'] = AUdailytests['new_cases'].diff()
 AUdailytests['DeltaCase'] = AUdailytests['DeltaCase'].fillna(0)
