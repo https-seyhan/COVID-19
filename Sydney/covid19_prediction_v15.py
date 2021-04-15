bc92b9f9d59a9e5c5f6da56175a4f13cb70396bc
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-16 07:27:44 +1000

    Update covid19_prediction_v15.py

--- Sydney/covid19_prediction_v15.py
+++ Sydney/covid19_prediction_v15.py
@@ -226,7 +226,6 @@ def highest_density_interval(pmf, p, debug=False):
                      index=[f'Low_{p * 100:.0f}',
                             f'High_{p * 100:.0f}'])
 
-
 print("Posteriors !!!!!!!", posteriors)
 hdi = highest_density_interval(posteriors, p=.9, debug=True)
 #Note that this takes a while to execute - it's not the most efficient algorithm
