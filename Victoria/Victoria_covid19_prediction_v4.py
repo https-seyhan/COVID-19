17b915c752053826068514cdcea147e34b071bb4
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-15 21:01:32 +1000

    Update Victoria_covid19_prediction_v4.py

--- Victoria/Victoria_covid19_prediction_v4.py
+++ Victoria/Victoria_covid19_prediction_v4.py
@@ -33,7 +33,6 @@ def getVicdata():
                                 min_periods=1,
                                 center=True).mean(std=2).round()
     
- 
     #posteriors, log_likelihood = get_posteriors(movingAverage, AUdailytests['newcasestotalratio'], sigma=.25)
     #get_posteriors(rolling, sigma=0.25)
 
