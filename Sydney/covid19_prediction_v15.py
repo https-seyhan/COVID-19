7f3b6784056174f6e68f62094a91e25f3d91c6e9
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-16 07:15:59 +1000

    Update covid19_prediction_v15.py

--- Sydney/covid19_prediction_v15.py
+++ Sydney/covid19_prediction_v15.py
@@ -112,7 +112,6 @@ movingAverage = plotNSWcases()
 print(" Len Moving Averages", len(movingAverage))
 
 def get_posteriors(ma, newtotalratio, sigma=0.15):
-
     GAMMA = 1 / 14 # 1 divided by the moving average
 
     # We create an array for every possible value of Rt
