17e281f190452a65dabc7aa7fd7ee3b6e401e816
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-18 20:36:14 +1000

    Update covid19_prediction_v19.py

--- Sydney/covid19_prediction_v19.py
+++ Sydney/covid19_prediction_v19.py
@@ -75,8 +75,6 @@ def plotNSWcases():
                                  min_periods=1,
                                  center=True).mean(std=2).round()
 
-    print("Rolling ", rolling.head(10))
-
     # Formatting
     ax.xaxis.set_major_locator(mdates.MonthLocator())
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
