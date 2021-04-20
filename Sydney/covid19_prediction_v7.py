53d85098a40e767853372bcaaba3a6a70145e357
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-20 23:08:02 +1000

    Update covid19_prediction_v7.py

--- Sydney/covid19_prediction_v7.py
+++ Sydney/covid19_prediction_v7.py
@@ -31,7 +31,6 @@ coronadata = coronadata[keep]
 postcodecount = coronadata['postcode'].unique()
 #print("post code count ", postcodecount)
 coronadata['notification_date'] = pd.to_datetime(coronadata['notification_date'])
-
 coronadata['cases'] = 1
 
 postcodecases = {}
