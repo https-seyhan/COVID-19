83b361cdb6ceb1062777080f3c40df7441d3722c
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-21 20:37:44 +1000

    Update covidtest.py

--- NLP/covidtest.py
+++ NLP/covidtest.py
@@ -58,7 +58,6 @@ with open('person.txt', 'w') as json_file:
   json.dump(person_dict, json_file)
 
 person_string = '{"name": "Bob", "languages": "English", "numbers": [2, 1.6, null]}'
-
 # Getting dictionary
 person_dict = json.loads(person_string)
 
