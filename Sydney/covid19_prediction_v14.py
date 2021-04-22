25d1e1bf68ed719977aedfdd87756a44b7bf677a
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-22 20:30:43 +1000

    Update covid19_prediction_v14.py

--- Sydney/covid19_prediction_v14.py
+++ Sydney/covid19_prediction_v14.py
@@ -112,7 +112,6 @@ def get_posteriors(ma, newtotalratio, sigma=0.15):
     print(" Len newtotalratio", len(newtotalratio))
 
     GAMMA = 1 / 14 # 1 divided by the moving average
-
     # We create an array for every possible value of Rt
     R_T_MAX = 6
     r_t_range = np.linspace(0, R_T_MAX, len(newtotalratio))
