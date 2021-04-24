f03757c8bb875a5cb82984240a3af3ed951d0009
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-24 19:00:45 +1000

    Update Victoria_covid19_prediction_v7.py

--- Victoria/Victoria_covid19_prediction_v7.py
+++ Victoria/Victoria_covid19_prediction_v7.py
@@ -41,7 +41,6 @@ def getVicdata():
     flattened.set_index('newDate2', inplace=True)
 
     vicdata = vicdata[['newDate2', 'VIC']]
-
     rolling = flattened.rolling(period,
                                 win_type='gaussian',
                                 min_periods=1,
