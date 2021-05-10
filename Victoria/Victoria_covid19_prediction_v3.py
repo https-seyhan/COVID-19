b2e1eaeb2a02d6b9d92e3e94645d5e0323903c01
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-05-10 22:01:16 +1000

    Update Victoria_covid19_prediction_v3.py

--- Victoria/Victoria_covid19_prediction_v3.py
+++ Victoria/Victoria_covid19_prediction_v3.py
@@ -36,7 +36,6 @@ def getVicdata():
                                 win_type='gaussian',
                                 min_periods=1,
                                 center=True).mean(std=2).round()
-
     plotVicCov19(flattened, rolling)
 
     print(" Len Moving Averages", len(rolling))
