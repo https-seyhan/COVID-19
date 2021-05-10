65bb989b8b973e1c4792603240e559beaff32679
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-05-10 22:26:11 +1000

    Update Victoria_covid19_prediction_v3.py

--- Victoria/Victoria_covid19_prediction_v3.py
+++ Victoria/Victoria_covid19_prediction_v3.py
@@ -111,7 +111,6 @@ def get_posteriors(ma, newtotalratio = [1,1], sigma=0.15):
     return posteriors, log_likelihood
 
 def plotVicCov19(flattened, rolling):
-
     fig, ax = plt.subplots(figsize=figsize)
     # Formatting
     ax.xaxis.set_major_locator(mdates.MonthLocator())
