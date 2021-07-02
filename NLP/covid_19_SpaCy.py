92b325a56425ef9743b4efee2863f42d5cbf3458
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-07-02 22:20:02 +1000

    Update covid_19_SpaCy.py

--- NLP/covid_19_SpaCy.py
+++ NLP/covid_19_SpaCy.py
@@ -102,7 +102,6 @@ def nlpWork(abstract, textcount):
 def coronaAnalysis(sha, abstract, count, textcount):
 	#doc = nlp(text)
 	textcount = 0
-
 	cleantext = [t.text for t in abstract if  not t.is_stop  and t.ent_type_ != 'GPE' ] # remove stop words. Exclude Geographic location
 
 	# convert list to nlp doc
