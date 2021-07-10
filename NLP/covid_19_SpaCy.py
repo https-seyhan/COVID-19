c8acea59ee8fed71844da946560425c0153a8f0b
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-07-11 09:59:14 +1000

    Update covid_19_SpaCy.py

--- NLP/covid_19_SpaCy.py
+++ NLP/covid_19_SpaCy.py
@@ -104,6 +104,7 @@ def coronaAnalysis(sha, abstract, count, textcount):
 	# convert list to nlp doc
 	cleandoc = Doc(nlp.vocab, words=cleantext)
 	matcher = Matcher(nlp.vocab)
+	
 	matcher.add("medicalcare", None, pattern1, pattern2, pattern3, pattern4, pattern5, pattern6,
 				pattern7, pattern8, pattern9, pattern10, pattern11, pattern12, pattern13, pattern14, pattern15,
 				pattern16, pattern17, pattern18, pattern19, pattern20, pattern21, pattern22)
